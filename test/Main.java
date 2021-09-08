4542049e3d3eb0b0f4fca7267579bc0994a7b22d
Niklas Kraßnig <niklas@krassnig.dev>
2021-09-08 11:42:21 +0200

    Make singular entry point for manual tests

---
+++ test/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+		AnimationTest.main(args);
+		CodeDrawTest.main(args);
+		EventTest.main(args);
+		GraphicTest.main(args);
+		TextFormatTest.main(args);
+	}
+}
