6292aa55248224c0e529cafe9796edcc60ee7c8a
Niklas Kraßnig <niklas@krassnig.dev>
2022-01-30 15:56:13 +0100

    Add documentation to the CodeDrawEventInvocationException

--- src/codedraw/CodeDrawEventInvocationException.java
+++ src/codedraw/CodeDrawEventInvocationException.java
@@ -1,5 +1,9 @@
 package codedraw;
 
+/**
+ * This exception is thrown when you try to use CodeDraw inside the CodeDraw event loop.
+ * Calling CodeDraw inside the event loop leads to concurrency issues and blocks the event loop.
+ */
 public class CodeDrawEventInvocationException extends RuntimeException {
 	CodeDrawEventInvocationException() {
 		super("CodeDraw methods should not be called inside of events!");
