6176dea754e93d91686518e38e2c4dde5f2525db
Niklas Kraßnig <niklas@krassnig.dev>
2022-02-04 21:57:11 +0100

    Rewrite GameOfLife using only structured programming and simplify

--- examples/GameOfLife.java
+++ examples/GameOfLife.java
@@ -1,110 +1,50 @@
-import codedraw.*;
-import codedraw.events.EventScanner;
-import codedraw.events.MouseDownEventArgs;
-import codedraw.events.MouseMoveEventArgs;
+import codedraw.CodeDraw;
+import codedraw.Palette;
+import codedraw.events.*;
 
 public class GameOfLife {
+	private static final int FIELD_SIZE = 10;
+
 	public static void main(String[] args) {
-		GameOfLife gof = new GameOfLife(6152, 6, 10);
+		final int size = 1 << 6; // must be power of 2
+		final int mask = size - 1;
 
-		for (int i = 0; true; i++) {
+		CodeDraw cd = new CodeDraw(size * FIELD_SIZE, size * FIELD_SIZE);
+		EventScanner es = new EventScanner(cd);
+
+		boolean[][] field = createRandomBooleans(size);
+
+		boolean isMouseDown = false;
+		boolean setValue = false;
+
+		for (int i = 0; !es.isClosed(); i++) {
+			while (es.hasEventNow()) {
+				if (es.hasMouseDownEvent()) {
+					MouseDownEventArgs a = es.nextMouseDownEvent();
+					isMouseDown = true;
+					setValue = !field[a.getX() / FIELD_SIZE][a.getY() / FIELD_SIZE];
+				}
+				else if (es.hasMouseUpEvent() || es.hasMouseLeaveEvent()) {
+					es.nextEvent();
+					isMouseDown = false;
+				}
+				else if (es.hasMouseMoveEvent()) {
+					MouseMoveEventArgs a = es.nextMouseMoveEvent();
+					if (isMouseDown) {
+						field[a.getX() / FIELD_SIZE][a.getY() / FIELD_SIZE] = setValue;
+					}
+				}
+				else {
+					es.nextEvent();
+				}
+			}
+
 			if (i % 8 == 0) {
-				// will simulate a new generation every 128ms
-				gof.simulate(1);
+				field = simulateNextGeneration(field, mask);
 			}
 
-			// renders about 60 frames per second
-			gof.processEvents();
-			gof.renderAndWait(16);
+			render(cd, field);
 		}
-	}
-
-	public GameOfLife(int rule, int logSize, int fieldSize) {
-		this.rule = rule;
-		size = 1 << logSize;
-		mask = size - 1;
-		this.fieldSize = fieldSize;
-
-		cd = new CodeDraw(size * fieldSize, size * fieldSize);
-		esc = new EventScanner(cd);
-		field = createRandomBooleans(size);
-	}
-
-	private final CodeDraw cd;
-	private final EventScanner esc;
-	private final int rule;
-	private final int size;
-	private final int mask;
-	private final int fieldSize;
-
-	private boolean[][] field;
-	private boolean isMouseDown = false;
-	private boolean setValue = false;
-
-	public void processEvents() {
-		while (esc.hasEventNow()) {
-			if (esc.hasMouseDownEvent()) {
-				MouseDownEventArgs a = esc.nextMouseDownEvent();
-				isMouseDown = true;
-				setValue = !field[a.getX() / fieldSize][a.getY() / fieldSize];
-			}
-			else if (esc.hasMouseUpEvent() || esc.hasMouseLeaveEvent()) {
-				esc.nextEvent();
-				isMouseDown = false;
-			}
-			else if (esc.hasMouseMoveEvent()) {
-				MouseMoveEventArgs a = esc.nextMouseMoveEvent();
-				if (isMouseDown) {
-					field[a.getX() / fieldSize][a.getY() / fieldSize] = setValue;
-				}
-			}
-			else {
-				esc.nextEvent();
-			}
-		}
-	}
-
-	public void renderAndWait(int milliSeconds) {
-		for (int x = 0; x < size; x++) {
-			for (int y = 0; y < size; y++) {
-				cd.setColor(field[x][y] ? Palette.BLACK : Palette.WHITE);
-				cd.fillSquare(fieldSize * x, fieldSize * y, fieldSize);
-			}
-		}
-
-		cd.show(milliSeconds);
-	}
-
-	public void simulate(int generations) {
-		for (int g = 0; g < generations; g++) {
-			boolean[][] nextField = new boolean[size][size];
-
-			for (int x = 0; x < size; x++) {
-				for (int y = 0; y < size; y++) {
-					nextField[x][y] = filterLifeLike(rule, field[x][y], new boolean[]{
-							field[(x + 1) & mask][(y + 1) & mask],
-							field[(x + 1) & mask][(y) & mask],
-							field[(x + 1) & mask][(y - 1) & mask],
-							field[(x) & mask][(y + 1) & mask],
-
-							field[(x) & mask][(y - 1) & mask],
-							field[(x - 1) & mask][(y + 1) & mask],
-							field[(x - 1) & mask][(y) & mask],
-							field[(x - 1) & mask][(y - 1) & mask],
-					});
-				}
-			}
-
-			field = nextField;
-		}
-	}
-
-	private static boolean filterLifeLike(int rule, boolean center, boolean[] n) {
-		int sum = 0;
-		for (int i = 0; i < n.length; i++) {
-			sum += n[i] ? 1 : 0;
-		}
-		return ((rule >> (sum + (9 * (center ? 1 : 0)))) & 1) == 1;
 	}
 
 	private static boolean[][] createRandomBooleans(int size) {
@@ -117,5 +57,44 @@ public class GameOfLife {
 		}
 
 		return field;
+	}
+
+	public static boolean[][] simulateNextGeneration(boolean[][] field, int mask) {
+		final int radius = 1;
+		boolean[][] nextField = new boolean[field.length][field.length];
+
+		for (int x = 0; x < field.length; x++) {
+			for (int y = 0; y < field[x].length; y++) {
+				int sum = 0;
+
+				for (int xn = -radius; xn <= radius; xn++) {
+					for (int yn = -radius; yn <= radius; yn++) {
+						if ((xn != 0 || yn != 0) && field[(x + xn) & mask][(y + yn) & mask]) {
+							sum++;
+						}
+					}
+				}
+
+				if (field[x][y]) {
+					nextField[x][y] = sum == 2 || sum == 3;
+				}
+				else {
+					nextField[x][y] = sum == 3;
+				}
+			}
+		}
+
+		return nextField;
+	}
+
+	public static void render(CodeDraw cd, boolean[][] field) {
+		for (int x = 0; x < field.length; x++) {
+			for (int y = 0; y < field[x].length; y++) {
+				cd.setColor(field[x][y] ? Palette.BLACK : Palette.WHITE);
+				cd.fillSquare(FIELD_SIZE * x, FIELD_SIZE * y, FIELD_SIZE);
+			}
+		}
+
+		cd.show(16);
 	}
 }
